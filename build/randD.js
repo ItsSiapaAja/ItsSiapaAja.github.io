e85a422dc383151d3a3fbe4e458b7ac729e86cff
ItsSiapaAja <farandkenzie2000@gmail.com>
2022-09-23 17:42:14 +0700

    Fixing some bugs

--- build/randD.js
+++ build/randD.js
@@ -2,6 +2,9 @@ let headerClick = document.querySelector(".-h-ll-click");
 let list_link = document.querySelector(".list-link");
 let ll_dropdown = "-ll-dropdown";
 
-headerClick.addEventListener("click", function () {
-    list_link.classList.toggle(ll_dropdown);
-});
+if((list_link && headerClick) != null)
+{
+    headerClick.addEventListener("click", function () {
+        list_link.classList.toggle(ll_dropdown);
+    });
+}
